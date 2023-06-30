06ed8b22a3921a8077e3a541cce4cdac17e87008
LUCAS BRITO <lucas.sapobr@gmail.com>
2023-06-30 04:59:16 -0400

    nova modificação

---
+++ controllers/user.js
@@ -0,0 +1,18 @@
+
+
+import {db} from "../db.js";
+
+export const getUsers = (_, res) => {
+ const q = "select * from usuarios";
+ 
+ db.query(q, (err, data) =>{
+
+    if (err) return res.json(err);
+    return res.status(200).json(data);
+ });
+
+
+ 
+ 
+};
+
---
+++ db.js
@@ -0,0 +1,17 @@
+
+
+import mysql from "mysql";
+
+
+
+
+export const db = mysql.createConnection({
+
+
+  host: "containers-us-west-183.railway.app",
+  user: "root",
+  password: "0y1vYFlFVLSbwVDLsDNG",
+  database: "railway"
+});
+
+
